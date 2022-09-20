3c57de8edb48a3bb459971dc3635437e0bddfbd0
Nils Part <Nils.Part@wow-group.com>
2022-09-20 14:25:05 +0200

    Added cart services, and started working on the cart and checkout

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,8 +13,15 @@ export class ProductDetailsComponent implements OnInit {
 
   product: Product | undefined;
 
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+
   // Here we inject ActivatedRoute into ctor
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private cartService: CartService
+    ) { }
 
   ngOnInit(): void {
 
